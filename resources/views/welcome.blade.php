3d862614dc24f7b713bc543130e1212357ece81e
Mariadenita <mariadenitaa20@gmail..com>
2024-07-22 19:58:42 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+        
+    </head>
+
+    <body>
+    <nav class="navbar navbar-expand-lg ">
+      <div class="container-fluid">
+        <div>
+            <img src="./images/logo.jpg">
+        </div>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" class=" justify-content-end">
+          <ul class="nav justify-content-end">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle nav-text" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Resources
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#"></a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-text" href="#">Success stroies</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle nav-text" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Company
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link nav-text " aria-disabled="true">Pricing</a>
+            </li>
+          </ul>
+          <form>
+            <button class="btn btn-outline-success second" >login</button>
+            <button class="btn btn-success prime " >create free account</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+        
+    <div class="first-main d-flex justify-content-end">
+      <div class="first-cont">
+        <span class="head1 head">Meet your<span class="fav"> favourite</span> new (old) Marketing channel</span>
+        <span class="sub-cont">Remarkable result. Easier than email Postcard marketing reinvented for modern ecommerce</span>
+        <div class="rate">
+          <button class="btn btn-success prime" >TRY IT RISK-FREE</button>
+          <div class="container mt-5">
+            <div class="d-flex align-items-center">
+              <div class="text-warning mr-2">
+              <i class="fa-solid fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <span>5.0 Shopify Rating</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="img-cont">
+        <img class="img1" src="./images/card.svg" alt="img">
+        <img class="img2" src="./images/card2.svg" alt="img">
+      </div>
+</div>
+
+    <div class="second-main">
+        <div class="text-center">
+            <div class="head">Trusted by thousands </div>
+            <div class="head">of top DTC<span class="fav"> brands </span></div>
+        </div>
+        <div class="row">
+            <div class="col-3 d-flex flex-column brand">
+            <span class="brand-img"><img src="./images/brand1.png" alt="img"></span>
+            <span class="brand-text"><img src="./images/t1.png" alt="img"></span>
+            </div>
+            <div class="col-3 d-flex flex-column brand">
+            <span class="brand-img"><img src="./images/brand2.png" alt="img"></span>
+            <span class="brand-text"><img src="./images/t2.png" alt="img"></span>
+            </div>
+            <div class="col-3 d-flex flex-column brand">
+            <span class="brand-img"><img src="./images/brand3.png" alt="img"></span>
+            <span class="brand-text"><img src="./images/t3.png" alt="img"></span>
+            </div>
+            <div class="col-3 d-flex flex-column brand">
+            <span class="brand-img"><img src="./images/brand4.png" alt="img"></span>
+            <span class="brand-text"><img src="./images/t4.png" alt="img"></span>
+            </div>
+        </div>
+    </div>
+<div class=" text-center third">
+    <div class="head ">Why use direct mail?</div>
+    <div class="head">It<span class="fav"> works </span>like crazy.</div>
+    <div class="row mt-5">
+      <div class="col-md-4">
+        <span class="head">28X</span>
+        <p>Higher response rate than email & digital</p>
+      </div>
+      <div class="col-md-4">
+      <img src="./images/mail.svg" alt="">
+        <p>Your messages get read</p>
+      </div>
+      <div class="col-md-4">
+        <span class="head">17 Days</span>
+        <p>Lifespan of a postcard vs. seconds for email or SMS</p>
+      </div>
+    </div>
+  </div>
+  <div class="postpilot d-flex justify-content-end">
+      <div class="first-cont">
+        <span class=" head text-white">Try PostPilot Risk-Free</span>
+        <span class="text-white">No contracts. No commitments. Guaranteed results.*</span>
+        <div class="rate">
+          <button class="btn btn-success second text-dark" >TRY IT RISK-FREE</button>
+          <div class="container mt-5">
+            <div class="d-flex align-items-center">
+              <div class="text-warning mr-2">
+              <i class="fa-solid fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <span class="text-white">5.0 Shopify Rating</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="img-cont">
+        <img  src="./images/postpilot.svg" alt="img">
+        
+      </div>
+</div>
+  <div class="last text-center">
+    <div class="head">
+        Latest trends & <span class="fav">insights</span>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <div class="brand">
+                <img src="./images/trend1.png" class="card-img-top" alt="...">
+                <div class="brand-text">
+                    <h5 class="card-title">3 Low-Cost Campaigns You Must Run to Win BFCM</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4 ">
+            <div class="brand">
+                <img src="./images/trend2.png" class="card-img-top" alt="...">
+                <div class="brand-text" >
+                    <h5>Postcard Marketing: The Definitive Guide to High ROI [2022]</h5>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="brand">
+                <img src="./images/trend3.png" class="card-img-top" alt="...">
+                <div class="brand-text">
+                    <h5>The Ultimate Guide to Thank You Notes for Ecommerce</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  <footer class="footer">
+        <div class="second-main">
+            <div class="row">
+                <div class="col-md-4 text-center text-md-left mb-4 mb-md-0">
+                    <div class="logo">
+                        <img src="./images/eco.svg" alt="ECO Friendly">
+                    </div>
+                    <p>Our paper is sourced from FSC-certified mills. We plant enough trees to more than double our paper usage.</p>
+                </div>
+                <div class="col-md-2">
+                    <h5 class="column-title">Company</h5>
+                    <ul class="list-unstyled">
+                        <li>About us</li>
+                        <li>Partner program</li>
+                        <li>Career</li>
+                        <li>Contact us</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h5 class="column-title"> </h5>
+                    <ul class="list-unstyled">
+                        <li>Pricing</li>
+                        <li>Reviews</li>
+                        <li>Direct Mail Academy</li>
+                        <li>Success stories</li>
+                        <li>Terms & conditions</li>
+                    </ul>
+                </div>
+                <div class="col-md-4 text-center text-md-left">
+                    <h5 class="column-title">Contact</h5>
+                    <ul class="list-unstyled">
+                        <li> support@postpilot.com</li>
+                        <li>Printed with ❤️ at our facility in South Carolina.</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="row mt-4">
+                <div class="col-12 text-center d-flex justify-content-between">
+                  <div>
+                    <div class="d-inline-block">
+                        <img src="./images/logo.jpg" alt="PostPilot">
+                    </div>
+                    <p>© 2022 PostPilot, Inc. All rights reserved.</p>
+                  </div>
+                    <div>
+                        <button class="btn btn-outline-success second">Login</button>
+                        <button class="btn btn-success prime">Try it for free</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+  
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <!-- <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script> -->
+    </body>
+</html>
